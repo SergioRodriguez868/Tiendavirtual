d9121cd8712e019b50c47c51c6243b1b08ac0def
SergioRodriguez868 <djchecho8@gmail.com>
2021-09-29 22:39:26 -0500

    Registros y login

---
+++ Login/con_db.php
@@ -0,0 +1,3 @@
+<?php
+$conexion = mysql_connect("localhost","root","Sa98070610868rd","log_prueba");
+?>
---
+++ Login/log_prueba.php
@@ -0,0 +1,9 @@
+<?php
+
+include("con_db.php");
+
+if(strlen(post['user'])<1 &&
+	strlen(post['user'])<1){
+}
+
+?>
